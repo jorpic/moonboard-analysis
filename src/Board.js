acc522e82934e5b88fb4983e45f943fe3208d61c
Max Taldykin <jorpic@gmail.com>
2020-03-02 19:16:16 +0300

    Extract Board component

---
+++ src/Board.js
@@ -0,0 +1,60 @@
+import { h, render, Component, createRef } from "preact";
+import board_2017 from "../images/2017.png";
+
+
+export default class Board extends Component {
+  constructor() {
+    super();
+    this.cnvRef = createRef();
+  }
+
+  componentDidMount() {
+    const cnv = this.cnvRef.current;
+    this.ctx = cnv.getContext("2d");
+    const img = new Image(0, 0);
+    const board = this;
+    img.onload = function() {
+      cnv.width = this.naturalWidth;
+      cnv.height = this.naturalHeight;
+      board.ctx.drawImage(this, 0, 0);
+      board.componentDidUpdate();
+    }
+    img.src = board_2017;
+  }
+
+  componentDidUpdate() {
+    const cnv = this.cnvRef.current;
+    const m = this.props.moves;
+
+    let color = "#0f0";
+    let i = 0, j = 0;
+    while(j < m.length) {
+      while(j < m.length && m[j] != " " && m[j] != "#") j++;
+      this.circle(m.substring(i, j), color);
+      if(m[j] == '#') { // select next color
+        color = color == "#0f0" ? "#00f" : "#f00";
+      }
+      i = ++j;
+    }
+  }
+
+  circle(pos, color) {
+    const i = pos.charCodeAt(0) - 65; // 65 = "A"
+    const j = 18 - pos.slice(1);
+
+    this.ctx.lineWidth = 6;
+    this.ctx.strokeStyle = color;
+    this.ctx.beginPath();
+    this.ctx.arc(
+      95 + i*50 ,
+      85 + j*50,
+      20, 0, 2 * Math.PI, false);
+    this.ctx.stroke();
+  }
+
+
+  render() {
+    return (
+      <canvas ref={this.cnvRef}/>);
+  }
+}
